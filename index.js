b0b1764abac6387d1c1fdc1aa70256faddd34023
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-09-19 11:15:44 +0530

    Setup database

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 // ! NPM  Modules imports
 const express = require('express');
+const cors = require('cors');
+
+// ! Util Imports
+const DB_CONNECT = require('./utils/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server Started at ${PORT}`);
-});
+// ! App Middlewares
+app.use(cors());
+
+DB_CONNECT(process.env.A_DB_URI)
+  .then(() => {
+    console.log(`DB Connected`);
+    return app.listen(PORT);
+  })
+  .then(() => {
+    console.log(`Server Started at ${PORT}`);
+  })
+  .catch((err) => {
+    console.log('Could not connect to database');
+  });
