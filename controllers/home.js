086dd9125315bdc35f8b8f036deb055bb8f46468
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-09-21 20:53:48 +0530

    building homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,30 @@
 const User = require('./../models/user.model');
 
-const getHome = (req, res) => {
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+const getHome = async (req, res) => {
+  // const allUsers = await User.find({}).select('_id');
+  // console.log(allUsers);
+  // const { followers } = await User.findById(req.user._id).select(
+  //   'followers -_id'
+  // );
+  // const suggestedFollowers = allUsers.filter((x) => !followers.includes(x));
+  // console.log(followers);
+
+  const allUsers = await await User.find({}).select(
+    'username -_id firstname lastname profileUrl'
+  );
+  shuffleArray(allUsers);
   res.render('home', {
     user: req.user,
+    suggested: allUsers.slice(0, 10),
   });
 };
 
