810f613bc8156841d70159619172209e1bb6974c
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-09-22 21:35:30 +0530

    imporvised text search

--- routes/search.js
+++ routes/search.js
@@ -6,20 +6,46 @@ const User = require('./../models/user.model');
 router.get('/search', async (req, res) => {
   // console.log(req.query.query);
 
-  const searchResults = await User.find({
-    $or: [
-      { firstname: new RegExp(`.*${req.query.query}.*`, 'ig') },
-      { lastname: RegExp(`.*${req.query.query}.*`, 'ig') },
-    ],
-  }).select('firstname lastname username profileUrl');
+  if (!req.query.query.includes(' ')) {
+    const searchResults = await User.find({
+      $or: [
+        {
+          firstname: new RegExp(`.*${req.query.query}.*`, 'ig'),
+        },
+        { lastname: new RegExp(`.*${req.query.query}.*`, 'ig') },
+      ],
+    }).select('firstname lastname username profileUrl');
 
-  // console.log(searchResults);
+    // console.log(searchResults);
 
-  res.render('search/search', {
-    query: req.query.query,
-    user: req.user,
-    searchResults,
-  });
+    res.render('search/search', {
+      query: req.query.query,
+      user: req.user,
+      searchResults,
+    });
+  } else {
+    const q1 = req.query.query.split(' ')[0];
+    const q2 = req.query.query.split(' ')[1];
+
+    const searchResults = await User.find({
+      $or: [
+        {
+          firstname: new RegExp(`.*${q1}.*`, 'ig'),
+        },
+        { lastname: new RegExp(`.*${q1}.*`, 'ig') },
+        {
+          firstname: new RegExp(`.*${q2}.*`, 'ig'),
+        },
+        { lastname: new RegExp(`.*${q2}.*`, 'ig') },
+      ],
+    }).select('firstname lastname username profileUrl');
+
+    res.render('search/search', {
+      query: req.query.query,
+      user: req.user,
+      searchResults,
+    });
+  }
 });
 
 module.exports = router;
--- tests/0213220921.js
+++ tests/0213220921.js
@@ -1,4 +1,4 @@
-const query = 'ritu';
+const query = 'ritu warwatkar';
 
 const x = new RegExp(`.*${query}.*`, 'ig');
 console.log(x);
